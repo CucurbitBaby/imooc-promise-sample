f28e4d1959b19129045a3f28c17728ee52684f06
cucurbitbaby <cucurbitbaby@163.com>
2019-09-22 23:23:31 +0800

    add async/await desc

--- sample/await.js
+++ sample/await.js
@@ -15,3 +15,10 @@ async function f1() {
     console.log(x); // 10
 }
 f1();   // f1()这样去调用异步函数的时候 返回值也是一个Promise
+
+
+// async用来表示函数是异步的，定义的函数会返回一个promise对象，可以使用then方法添加回调函数。
+//    既然async 定义的方法返回的是 Promise对象。 用返回Promise的方式，await也可以获取数据，并不一定要是定义的async方法await才能获取数据
+
+// await 可以理解为是 async wait 的简写。await 必须出现在 async 函数内部，不能单独使用。
+//    await 阻塞的功能 ，把异步改成一个同步
